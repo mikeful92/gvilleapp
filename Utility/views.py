ff5370c40731135311de64e255cb8acf90fd186b
Michael Cordoba <mikeful92@hotmail.com>
2015-05-18 22:53:15 +0000

    Added clean input function

--- Utility/views.py
+++ Utility/views.py
@@ -5,6 +5,31 @@ from django.shortcuts import render, get_list_or_404, HttpResponseRedirect
 from .forms import AddressForm
 from .models import Consumption
 
+
+def cleanData(address):
+    address = address.upper()
+    notAllowed = ['GAINESVILLE', 'FL', 'FLORIDA', ',']
+    for word in notAllowed:
+        if word in address:
+            address = address.replace(word, '')
+    addrDict = {"NORTHWEST": "NW",
+                "NORTHEAST": "NE",
+                "SOUTHWEST": "SW",
+                "SOUTHEAST": "SE",
+                "TERRACE": "TER",
+                "STREET": "ST",
+                "AVENUE": "AVE",
+                "PLACE": "PL",
+                "BOULEVARD": "BLVD",
+                "DRIVE": "DR",
+                "CIRCLE": "CIR",
+                "COURT": "CT",
+    }
+    for key, value in addrDict.iteritems():
+        if key in address:
+            address = address.replace(key, value)
+    return address
+
 def home(request):
     form = AddressForm(None)
     context = {"form": form}
@@ -15,6 +40,7 @@ def results(request):
     form = AddressForm(request.POST or None)
     if form.is_valid():
         address = form.cleaned_data['address']
+        address = cleanData(address)
         obj = get_list_or_404(Consumption, ServiceAddress=address)
     context = {"form": form, "obj": obj}
     template = "results.html"
