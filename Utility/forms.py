a9411b88d90f4ad24187c613b0e94d94c5fd9524
Michael Cordoba <mikeful92@hotmail.com>
2015-05-29 22:04:27 +0000

    Added input placeholder and github link

--- Utility/forms.py
+++ Utility/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class AddressForm(forms.Form):
-    address = forms.CharField(label="Street Address", max_length=120, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    address = forms.CharField(label="Street Address", max_length=120, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex. 758 NW 18TH TER'}))
